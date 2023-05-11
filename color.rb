408bfd076b8ebdc19616e9448234ef57a00805f0
Lovro Zelić <lovro.zelic@icloud.com>
2023-05-11 10:59:12 +0200

    Add an Instance of the class

--- color.rb
+++ color.rb
@@ -1,4 +1,11 @@
 # defining a color class
 class Color
 
+  def initialize(name, hexcode)
+    # Do cool things with name and hexcode
+    puts "The color #{name} has hexcode #{hexcode}"
+  end
 end
+
+color1 = Color.new("purple", "#8824a4")
+color2 = Color.new("blue", "#4x6fcc")
