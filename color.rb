43d011a68980ee69a234289613f2b0de06149cc5
Lovro Zelić <lovro.zelic@icloud.com>
2023-05-11 11:04:23 +0200

    instance variables

--- color.rb
+++ color.rb
@@ -2,10 +2,22 @@
 class Color
 
   def initialize(name, hexcode)
-    # Do cool things with name and hexcode
-    puts "The color #{name} has hexcode #{hexcode}"
+    @name = name
+    @hexcode = hexcode
+  end
+
+  def name
+    @name
+  end
+
+  def hexcode
+    @hexcode
   end
 end
 
-color1 = Color.new("purple", "#8824a4")
-color2 = Color.new("blue", "#4x6fcc")
+color = Color.new("purple", "#8824a4")
+
+# color1 = Color.new("purple", "#8824a4")
+# color2 = Color.new("blue", "#4x6fcc")
+
+puts "The hexcode of #{color.name} is #{color.hexcode}"
